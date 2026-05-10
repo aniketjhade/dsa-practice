153ceceb5c09e42c2abad704e2336e3a3ed4aaaa
aniketjhade <aniketjhade2012@gmail.com>
2026-05-11 01:51:37 +0530

    Count digit problem

---
+++ Warm-up/count-digit.js
@@ -0,0 +1,15 @@
+const n = 42340;
+
+function countDigit(n) {
+  if (n === 0) return 1;
+  let count = 0;
+
+  n = Math.abs(n);
+  while (n > 0) {
+    n = Math.floor(n / 10);
+    count++;
+  }
+  return count;
+}
+
+console.log(countDigit(n));
