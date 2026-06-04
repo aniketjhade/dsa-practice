cdc773ee4c1fd23c252de5006f117040323c1e9c
aniketjhade <aniketjhade2012@gmail.com>
2026-06-04 13:30:48 +0530

    Binary search

---
+++ Searching&Sorting/binary-search.js
@@ -0,0 +1,22 @@
+function binarySearch(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (right >= left) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] == target) {
+      return mid;
+    } else if (target > nums[mid]) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+}
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const target = 5;
+const result = binarySearch(nums, target);
+console.log(result);
