6781ff9f1a900289141ec48f47bb7bd269d7dc86
aniketjhade <aniketjhade2012@gmail.com>
2026-06-06 23:05:59 +0530

    updated nbput

--- Searching&Sorting/binary-search.js
+++ Searching&Sorting/binary-search.js
@@ -16,7 +16,7 @@ function binarySearch(nums, target) {
   return -1;
 }
 
-const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 const target = 5;
 const result = binarySearch(nums, target);
 console.log(result);
