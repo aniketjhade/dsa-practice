cc8022d3caf440ba800858bc57229db50816ab65
aniketjhade <aniketjhade2012@gmail.com>
2026-06-28 00:00:44 +0530

    bubble sort

--- Searching&Sorting/bubbleSort.js
+++ Searching&Sorting/bubbleSort.js
@@ -1,24 +1,22 @@
 function bubbleSort(arr) {
-    let n = arr.length;
+  let n = arr.length;
 
-    for (let i = 0; i < n - 1; i++){
-        let isSwapped = false;
-        for (let j = 0; j < n - 1 - i; j++){
-
-            if (arr[j] > arr[j + 1]) {
-
-                let temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-                isSwapped = true;
-            }
-        }
-        if (!isSwapped) break;
+  for (let i = 0; i < n - 1; i++) {
+    let isSwapped = false;
+    for (let j = 0; j < n - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        isSwapped = true;
+      }
     }
-    return arr;
+    if (!isSwapped) break;
+  }
+  return arr;
 }
 
-const arr = [7, 5, 2, 1, 4, 8, 9];
+const arr = [7, 5, 2, 1, 3, 4, 8, 9];
 
 const output = bubbleSort(arr);
 
