7205301b3aef8cf09184c817d219924efc81c5b4
aniketjhade <aniketjhade2012@gmail.com>
2026-07-02 00:01:10 +0530

    Insertion Sort

---
+++ Searching&Sorting/insertionSort.js
@@ -0,0 +1,19 @@
+function insertionSort(arr) {
+  let n = arr.length;
+
+  for (let i = 1; i < n; i++) {
+    let curr = arr[i];
+    let prev = i - 1;
+
+    while (arr[prev] > curr && prev >= 0) {
+      arr[prev + 1] = arr[prev];
+      prev--;
+    }
+    arr[prev + 1] = curr;
+  }
+  return arr;
+}
+
+const arr = [7, 2, 8, 0, 1, 4, 6, 5];
+const output = insertionSort(arr);
+console.log(output);
